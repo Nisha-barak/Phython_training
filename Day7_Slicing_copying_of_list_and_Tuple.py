b2ddfab8947dd335bd00b99902106326363c5c16
Nisha-barak <60150004+Nisha-barak@users.noreply.github.com>
2020-01-29 21:54:16 +0530

    Add files via upload

---
+++ Day7_Slicing_copying_of_list_and_Tuple.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+numbers=list(range(1,21))
+print(numbers)
+
+
+# In[2]:
+
+
+#Req: small,large and sum of total num
+min(numbers)
+
+
+# In[3]:
+
+
+max(numbers)
+
+
+# In[4]:
+
+
+sum(numbers)
+
+
+# In[ ]:
+
+
+#slicing of list:
+
+
+# In[6]:
+
+
+students=['sumanth','prashanth','ashok','manem','chintu','azeem']
+
+
+# In[ ]:
+
+
+#Req:2 Groups
+
+
+# In[7]:
+
+
+print(students[2])
+
+
+# In[8]:
+
+
+print(students[0:3])
+
+
+# In[9]:
+
+
+print(students[3:6])
+
+
+# In[ ]:
+
+
+#Copying a list:
+
+
+# In[11]:
+
+
+my_food=['pizza','burger','juice']
+friends_food=my_food[:]
+
+
+# In[12]:
+
+
+print(friends_food)
+
+
+# In[ ]:
+
+
+#Introdution to tuple datatype
+--Is an imutable datatype
+
+
+# In[ ]:
+
+
+#how to define a tuple()
+
+
+# In[13]:
+
+
+dimensions=(200,50)
+print(dimensions)
+
+
+# In[14]:
+
+
+type(dimensions)
+
+
+# In[ ]:
+
+
+#pep8
+
+
+# In[ ]:
+
+
+Python Enhancement proposals:
+    1.Proper indentation ---->Ex---->For loop
+    2. Line length  --->80 words max
+    3. Blank line spaces
+
