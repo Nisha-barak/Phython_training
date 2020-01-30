57cf33be5446f24561bc57578d0df0ce0f4c9ee5
Nisha-barak <60150004+Nisha-barak@users.noreply.github.com>
2020-01-31 00:35:32 +0530

    Add files via upload

---
+++ Day_8_Dictionary.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Introduction to Dictionaries:
+
+
+# In[ ]:
+
+
+Key-value pair combination:
+
+
+# In[ ]:
+
+
+How to define a dictionary: ------> {}
+    
+It is an mutable data type.
+
+
+# In[9]:
+
+
+Info={'First_name':'Nisha','Last_name':'Barak'}
+
+print(Info)
+
+type(Info)
+
+
+# In[ ]:
+
+
+#How to get the individual values on the console....?
+
+
+# In[10]:
+
+
+print(Info['First_name'])
+
+
+# In[ ]:
+
+
+#Enhancment of the code....!!
+
+
+# In[12]:
+
+
+name1=Info['First_name']
+name2=Info['Last_name']
+
+print(f"My first name is {name1} and last name is {name2}.")
+
+
+# In[ ]:
+
+
+#Starting with an empty dict:
+
+
+# In[14]:
+
+
+Fruits={}  
+
+Fruits['Apple']='Red'
+
+Fruits['Banana']='Yellow'
+
+print(Fruits)
+
+
+# In[ ]:
+
+
+#How to add new key value pairs to the above.
+
+
+# In[15]:
+
+
+Fruits['Orange']='Orange'
+
+Fruits['Chiku']='Brown'
+
+print(Fruits)
+
+
+# In[ ]:
+
+
+#Req:How to modify the values.
+
+
+# In[16]:
+
+
+Fruits['Apple']='Green'
+
+print(Fruits)
+
+
+# In[ ]:
+
+
+#Removing elements from the dict:
+
+
+# In[18]:
+
+
+del Fruits['Chiku']
+
+print(Fruits)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
