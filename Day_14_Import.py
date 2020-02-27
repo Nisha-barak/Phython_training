f960b932f0cd35e077e6e80640a2ae1670aa6779
Nisha-barak <60150004+Nisha-barak@users.noreply.github.com>
2020-02-27 21:35:50 +0530

    Add files via upload

---
+++ Day_14_Import.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import Day_14_Function_Dogname as dg
+
+
+# In[4]:
+
+
+my_dog=dg.Dog('Tommy',3)
+
+
+# In[6]:
+
+
+my_dog.sit()
+
+
+# In[7]:
+
+
+my_dog.roll_over()
+
+
+# In[ ]:
+
+
+
+
